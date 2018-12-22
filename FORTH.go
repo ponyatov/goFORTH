5368127654877ef7d5b859f2baf298a3f5253f00
Dmitry Ponyatov <dponyatov@gmail.com>
2018-12-22 16:17:55 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- FORTH.go
+++ FORTH.go
@@ -2,24 +2,73 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
-
-	"google.golang.org/appengine"
+	//	"google.golang.org/appengine"
 )
 
 var samara, utc *time.Location
+
+const html = `<html><head>
+	<title>FORTH/go</title>
+	<link rel="stylesheet" type="text/css" href="/static/css.css">
+	<link rel="manifest" href="/static/app.manifest">
+	<link rel="icon" href="/static/lego.png" type="image/png">
+	<meta name="theme-color" content="black">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>`
+
+const htlog = `
+</div>
+`
 
 func main() {
 
 	samara, _ = time.LoadLocation("Europe/Samara")
 	utc, _ = time.LoadLocation("UTC")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.HandleFunc("/", index)
-	appengine.Main()
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
+
+func manifest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, `
+{
+	"short_name": "FORTH/go",
+	"name": "FORTH/go",
+	"icons": [ { "src": "/static/lego.png", "type": "image/png" } ],	
+	"orientation": "portrait",
+	"display": "standalone",
+	"background_color": "black",
+	"theme_color": "black"
+}
+	`)
+}
+
+const pad = `
+<div>
+<form id=cmd action="" method=post novalidate>
+<textarea autocorrect="off" autofocus id=pad name="pad" rows="3"></textarea>
+<input id=go type=submit value="GO">
+</form>
+</div>
+`
 
 func index(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
-	fmt.Fprintf(w, "<html><h1>Превед медвед in <ul><li>%v</li><li>%v</li></ul>", now.In(samara), now.In(utc))
+	fmt.Fprintf(w, `
+	%s
+	<div id=log><h1>Превед медвед in <ul><li>%v</li><li>%v</li></ul></div>
+	%s
+	`, html, now.In(samara), now.In(utc), pad)
 }
--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 go: FORTH.go
 	gofmt -w $<
 	go run $<
+
+local:
+	python C:\Google\Cloud\SDK\google-cloud-sdk\bin\dev_appserver.py app.yaml 
+deploy:
+	gcloud app deploy 
+browse:
+	gcloud app browse
